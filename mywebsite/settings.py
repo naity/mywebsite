2ab459e69626c2e5b2678fbb94459f0b61eac3a5
ytian <ytian@uab.edu>
2017-03-20 18:49:45 -0700

    Fix S3

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -134,7 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # define MEDIA_ROOT and MEDIA_URL for uploading files
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = '/media/'
+#MEDIA_URL = '/media/'
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
