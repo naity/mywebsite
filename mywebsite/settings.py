c3dcea870614456d1decaa38476bc357d0926174
ytian <ytian@uab.edu>
2017-03-20 21:57:43 -0700

    Fix static files and s3

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -20,14 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '7dw*z+irrfrj2-o5o%h-hl0upx9)m2z5$84$qp^9#)_ijp(+xw'
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', )
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['floating-citadel-65305.herokuapp.com', "192.168.0.18"]
+ALLOWED_HOSTS = ['floating-citadel-65305.herokuapp.com', "192.168.0.18",
+                "127.0.0.1"]
 
 
 # Application definition
@@ -134,7 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # define MEDIA_ROOT and MEDIA_URL for uploading files
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-#MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
