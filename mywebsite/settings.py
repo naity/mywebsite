f6d8a08107e323e101a1efecd944756a035f9a73
ytian <ytian@uab.edu>
2017-03-20 18:29:12 -0700

    Fix s3 storage

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'home',
     'research',
     'photography',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -142,3 +143,12 @@ DATABASES['default'].update(db_from_env)
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+# for serving media files with Amazon S3
+AWS_QUERYSTRING_AUTH = False
+AWS_REGION = "us-west-2"
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
+MEDIA_URL = 'http://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
